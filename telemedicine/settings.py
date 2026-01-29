3896c14e489e4fa7eda6f9ff67f9a1e46b8ae48b
cofurfeast-prog <Rajeshilamion@gmail.com>
2026-01-29 14:00:54 +0545

    Fix Django admin static files with whitenoise

--- telemedicine/settings.py
+++ telemedicine/settings.py
@@ -143,9 +143,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/stable/howto/static-files/
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Add whitenoise for static files in production
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 # Media files
 MEDIA_URL = '/media/'
